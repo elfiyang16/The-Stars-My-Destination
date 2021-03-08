c15bfcf8b29a9c5e166b345862f411498765dad4
Elfi Yang <elfiyang16@gmail.com>
2021-03-08 11:09:22 +0000

    refactor: overwrite dragon field on capsule to get all data

--- server/resolvers/index.ts
+++ server/resolvers/index.ts
@@ -1,3 +1,5 @@
+import { find } from "lodash";
+
 export const resolvers = {
   Query: {
     capsules: async (_, args, { dataSources }): Promise<any> => {
@@ -35,6 +37,18 @@ export const resolvers = {
     ): Promise<any> => {
       const dragon = await dataSources.spaceXAPI.getDragonById(id);
       return dragon;
+    },
+  },
+  Capsule: {
+    /* overwrite the default dragon field implementation since default rest only return id field of dragon */
+    dragon: async (_, args, { dataSources }): Promise<any> => {
+      console.log(`fetch dragon ${_.id}`);
+      const dragons = await dataSources.spaceXAPI.getDragons({ ...args });
+      const dragonInCapsule = find(
+        dragons,
+        (dragon) => dragon.id === _.dragon.id
+      );
+      return dragonInCapsule;
     },
   },
 };
