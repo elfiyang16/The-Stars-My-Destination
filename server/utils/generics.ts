c78a0d3cf3ff57040a6a6f5dc50dade2ca8197a2
Elfi Yang <elfiyang16@gmail.com>
2021-03-30 22:36:10 +0100

    feat: add editor config

---
+++ server/utils/generics.ts
@@ -0,0 +1,14 @@
+export const PLAY_GROUND_SETTINGS = {
+  "editor.theme": "dark" as const,
+  "editor.reuseHeaders": true,
+  "editor.fontSize": 16,
+  "editor.fontFamily": `'Fira Code', 'Source Code Pro', 'Consolas'`,
+  "prettier.printWidth": 2,
+  "prettier.tabWidth": 2,
+  "prettier.useTabs": true,
+  "schema.polling.enable": true,
+  "schema.polling.interval": 5000,
+  "schema.disableComments": false,
+  "tracing.hideTracingResponse": false,
+  "queryPlan.hideQueryPlanResponse": false,
+};
